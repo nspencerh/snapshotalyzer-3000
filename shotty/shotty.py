b370f26929ffe2e73a217154237d8f294544b39d
Nicolas Spencer <nicolaspencer@gmail.com>
2018-04-01 09:54:45 -0300

    functionallity of checking if there is a pending snapshot when taking one for a volume is added.

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,6 +16,11 @@ def filter_instances(project):
         instances = ec2.instances.all()
 
     return(instances)
+#############VER SI EL VOLUMEN TIENE SNAPSHOTS PENDIENTES#######################
+def has_pending_snapshots(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+################################################################################
 ################################################################################
 @click.group()
 def cli():
@@ -157,6 +162,9 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
+            if has_pending_snapshots(v):
+                print(" Skipping {0}, snapshot already in progress".format(v.id))
+                continue
             print("  Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by SnapshotAlyzer")
 
